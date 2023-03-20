a9c0e99863684e7db52472509f23e393e8150f02
tony555t <tonytullah764@gmail.com>
2023-03-20 14:09:04 +0300

    model creation

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  resources :restaurants
+  resources :restaurant_pizzas
+  resources :pizzas
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
---
+++ db/migrate/20230320110452_create_restaurant_pizzas.rb
@@ -0,0 +1,9 @@
+class CreateRestaurantPizzas < ActiveRecord::Migration[7.0]
+  def change
+    create_table :restaurant_pizzas do |t|
+      t.integer :price
+
+      t.timestamps
+    end
+  end
+end
