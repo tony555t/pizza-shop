4dcae04030640bcac3433a4cdfe8da35e980ad33
tony555t <tonytullah764@gmail.com>
2023-03-20 16:44:29 +0300

    delete  end point

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,8 +1,27 @@
 class RestaurantsController < ApplicationController
     def index
       restaurants = Restaurant.all
+      render json: restaurants, status: :ok
+    end
   
-      render json: restaurants.as_json(only: [:id, :name, :address]), status: :ok
+    def show
+      restaurant = Restaurant.find_by(id: params[:id])
+      if restaurant
+        render json: restaurant.as_json(include: :pizzas), status: :ok
+      else
+        render json: { error: "Restaurant not found" }, status: :not_found
+      end
+    end
+  
+    def destroy
+      restaurant = Restaurant.find_by(id: params[:id])
+      if restaurant
+        restaurant.restaurant_pizzas.destroy_all
+        restaurant.destroy
+        head :no_content
+      else
+        render json: { error: "Restaurant not found" }, status: :not_found
+      end
     end
   end
   
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,5 @@
 class Restaurant < ApplicationRecord
     has_many :restaurant_pizzas
-    has_many :restaurants, through: :restaurant_pizzas
+    has_many :pizzas, through: :restaurant_pizzas
 
 end
